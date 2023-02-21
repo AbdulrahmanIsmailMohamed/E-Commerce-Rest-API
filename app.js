21c259ae4486e1677199bc51089517d4be8ebd2b
Abdulrahman Ismail <abdulrahman.ismail.mohammed@gmail.com>
2023-02-21 08:20:30 +0200

    init folders

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require("express");
 require("dotenv").config();
-const morgan = require("morgan");
+require("./db/connect")
 
 const app = express();
 
+// morgan
+const morgan = require("morgan");
+app.use(morgan("tiny")); // HTTP request logger middleware for node.js
+
 // middleware
-app.use(morgan("tiny")) // HTTP request logger middleware for node.js
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.post("/api/v1/products", (req, res) => {
-    const products = req.body;
-    res.json(products);
-})
+// routes
+const userRoute = require("./routes/users");
+const categoryRoute = require("./routes/categories");
+const orderRoute = require("./routes/orders");
+const productRoute = require("./routes/products");
+
+const api = process.env.API
+app.use(`${api}/user`, userRoute);
+app.use(`${api}/category`, categoryRoute);
+app.use(`${api}/order`, orderRoute);
+app.use(`${api}/product`, productRoute);
+
 
 // listening server
 const port = process.env.PORT || 3000;
---
+++ modules/product.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+
+});
+
+const product = mongoose.model("product", productSchema);
+
+module.exports = product;
